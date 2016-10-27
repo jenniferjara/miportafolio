6bc218fac8fcf16d8e43ee589348be600e6c06be
jenniferjara <lisbet.jara@gmail.com>
2016-10-27 11:11:05 -0500

    efectos a los iconos de skills - jquery

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-
+var movimiento = function(){
+	$(this).addClass("animated rubberBand");
+}
+var quitar = function(){
+	$(this).removeClass("animated rubberBand")
+}
+var iniciar = function(){
+	var skill = $(".iconos img");
+	skill.mouseenter(movimiento);
+	skill.mouseleave(quitar);
+}
+$(document).ready(iniciar);
 
