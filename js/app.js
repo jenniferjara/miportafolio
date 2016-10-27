797510d0034b2b6aee391cd64ed1928f8204dacb
jenniferjara <lisbet.jara@gmail.com>
2016-10-27 15:45:21 -0500

    cambio en navbar

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ var quitar = function(){
 	$(this).removeClass("animated rubberBand")
 }
 var iniciar = function(){
+	new WOW().init();
 	var skill = $(".iconos img");
 	skill.mouseenter(movimiento);
 	skill.mouseleave(quitar);
