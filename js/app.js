8f541152e9bdc16435bafc09db0b0f42c6d9d4e1
jenniferjara <lisbet.jara@gmail.com>
2016-10-28 13:00:53 -0500

    agregando particle.js en el hero

--- js/app.js
+++ js/app.js
@@ -5,15 +5,17 @@ var quitar = function(){
 	$(this).removeClass("animated rubberBand")
 }
 var iniciar = function(){
-	new WOW().init();
-	var skill = $(".iconos img");
-	skill.mouseenter(movimiento);
-	skill.mouseleave(quitar);
+	//----ABOUT ME
 	$("#changeWord").changeWords({
         time: 1000,
         animate: "bounce",
         selector: "span",
         repeat:true
       });
+	//-----SKILLS
+	var skill = $(".iconos img");
+	skill.mouseover(movimiento);
+	skill.mouseleave(quitar);
+	
 }
 $(document).ready(iniciar);
