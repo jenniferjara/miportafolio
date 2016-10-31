0fe545adb4aeeafbcdc7b50b3f6ff2fb94381b06
jenniferjara <lisbet.jara@gmail.com>
2016-10-30 23:11:27 -0500

    arregando contenido(texto)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+//---navbar
+var scroll_start = 0;
+var startchange = $('#cambio');
+var offset = startchange.offset();
+var alturaNav = $("header").outerHeight();
+
+var transparente = function(){
+	scroll_start = $(this).scrollTop();
+	if(scroll_start > offset.top) {
+		$(".navbar-default").css('background-color', '#3D5C4A');
+	} else {
+		$('.navbar-default').css('background-color', 'transparent');
+	}
+}
+//---secciones de navbar
+var secciones = function(){
+	var ref = $(this).attr("href");
+	
+	$("html body").animate({
+		scrollTop: $(ref).offset().top - alturaNav
+		}, 1000)
+}
+//----skills
 var movimiento = function(){
 	$(this).addClass("animated rubberBand");
 }
@@ -5,16 +28,22 @@ var quitar = function(){
 	$(this).removeClass("animated rubberBand")
 }
 var iniciar = function(){
-	//----ABOUT ME
+	//----navbar
+	if (startchange.length){
+		$(document).scroll(transparente);
+	}
+	//---secciones de navbar
+	$("header a").click(secciones);
+	//----about me
 	$("#changeWord").changeWords({
         time: 1000,
         animate: "bounce",
         selector: "span",
         repeat:true
       });
-	//-----SKILLS
+	//----skills
 	var skill = $(".iconos img");
-	skill.mouseover(movimiento);
+	skill.mouseenter(movimiento);
 	skill.mouseleave(quitar);
 	
 }
